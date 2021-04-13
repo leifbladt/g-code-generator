7f02b6361e07475f2c9b4544df589d9ad600ac63
Leif Bladt <leif.bladt@gmx.de>
2021-04-13 22:01:55 +0200

    Add simple generate button

---
+++ src/components/GCodeGenerator.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <h1>G-Code Generator</h1>
+        <p>
+            <button v-on:click="generate">Generate</button><br>
+            <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GCodeGenerator',
+  props: {
+      message: String
+  },
+  methods: {
+      generate: generate
+  }
+}
+
+function generate() {
+    this.message = `G0 Z10; Z at 10 mm
+
+M201 X750 Y750; acceleration
+
+M400; complete all moves in the planner
+G1 F3600
+
+G1 X25 Y25
+G1 X175 Y25
+G1 X175 Y175
+G1 X25 Y175
+G1 X25 Y25`;
+}
+</script>
